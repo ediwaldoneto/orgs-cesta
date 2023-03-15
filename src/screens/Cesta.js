87116bbcd15d475bdbe2dc530676eae7dbd0f74b
ediwaldo.neto <ediwaldo.neto@outlook.com>
2023-03-14 21:14:40 -0300

    componente texto criado

--- src/screens/Cesta.js
+++ src/screens/Cesta.js
@@ -3,25 +3,26 @@ import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 
 import topo from '../../assets/topo.png';
 import logo from '../../assets/logo.png'
+import Texto from '../components/Texto';
 
 const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
     return <>
         <Image source={topo} style={estilos.topo} />
-        <Text style={estilos.titulo}>Detalhe da Cesta</Text>
+        <Texto style={estilos.titulo}>Detalhe da Cesta</Texto>
         <View style={estilos.cesta}>
-            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <Texto style={estilos.nome}>Cesta de Verduras</Texto>
             <View style={estilos.fazenda}>
                 <Image source={logo} style={estilos.imagemFazenda} />
-                <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+                <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
             </View>
 
-            <Text style={estilos.descricao}>Uma cesta com produtos selecionados
+            <Texto style={estilos.descricao}>Uma cesta com produtos selecionados
                 cuidadosamente da fazenda direto para a
                 sua cozinha
-            </Text>
-            <Text style={estilos.preco}>R$ 40,00</Text>
+            </Texto>
+            <Texto style={estilos.preco}>R$ 40,00</Texto>
         </View>
     </>
 }
@@ -45,19 +46,18 @@ const estilos = StyleSheet.create({
         color: "#464646",
         fontSize: 26,
         lineHeight: 42,
-        fontFamily: "MontserratBold",
+        fontWeight: 'bold'
+
     },
     nomeFazenda: {
         fontSize: 16,
         lineHeight: 26,
         marginLeft: 12,
-        fontFamily: "MontserratRegular",
     },
     descricao: {
         color: "#A3A3A3",
         fontSize: 17,
         lineHeight: 26,
-        fontFamily: "MontserratBold",
     },
     preco: {
         color: "#2A9F85",
